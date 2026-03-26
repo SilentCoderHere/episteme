0d37bcefc30e3ed786ed9e1bb8f505e547957dc6
Aryan <aryanrajivyms@gmail.com>
2026-03-26 14:45:56 +0530

    Updated version to 1.0.39(40) (#115)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.aryan.reader"
         minSdk = 26
         targetSdk = 35
-        versionCode = 39
-        versionName = "1.0.38"
+        versionCode = 40
+        versionName = "1.0.39"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         externalNativeBuild {
